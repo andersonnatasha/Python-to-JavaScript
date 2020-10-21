7f5f9f45df3c81ebf45268a5613ef6e2121e612e
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-21 02:58:01 +0000

    revert to Arrow Function Expressions

--- MelonCounting.js
+++ MelonCounting.js
@@ -1,35 +1,33 @@
 
-const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
-                     'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
-                     'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
-                     'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
-                     'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
-                     'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
-                     'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
-                     'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
-                     'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
-                     'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
-                     'Watermelon', 'Santa Claus', 'Casaba'];
+// Our customers are going to buy lots of melons!
 
+const melonsToAdd= ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
 
-/**
- * Take in an array and return an object of # of melons by melon type.
- */
-function countMelons(melonsArray) {
+// Take in a list and return a dictionary of # of melons by melon type.
+const countMelons = (melonArrays) => {
   const melonCounts = {};
 
-  for (const melon of melonsArray) {
+  for (const melon of melonArrays) {
     if (melonCounts[melon]) {
-      melonCounts[melon] += 1;
-    } else {
-      melonCounts[melon] = 1;
+    melonCounts[melon] += 1;
+   } else {
+     melonCounts[melon] = 1;
     }
   }
-
   return melonCounts;
 }
 
-console.log(countMelons(melonsToAdd));
+console.log(countMelons(melonsToAdd))
 // def count_melons(melon_list):
 //     """Take in a list and return a dictionary of # of melons by melon type."""
 
